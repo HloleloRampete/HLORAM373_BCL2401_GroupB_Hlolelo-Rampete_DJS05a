16f79d237e7f8f75d00fd45974678a5b2032d6c5
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-21 14:46:28 +0200

    Initializing wind, temp and humidity

--- store.js
+++ store.js
@@ -31,3 +31,37 @@
 * @prop {Update} update
 * @prop {Subscribe} subscribe
 */
+
+const initial = {
+    wind: {
+        value: 1,
+    },
+    temperature: {
+        value: 1,
+    },
+    humidity: {
+        value: 1,
+    }
+};
+
+const createStore = (initial) => {
+    /**
+     *  @type {Array<State>}
+     */
+    const states = [initial]
+
+    const update = (action) => {
+        if (typeof action !== 'function') {
+            throw new Error ("action is requred to be function")
+        }
+    }
+
+    const subscribe = () => {
+
+    }
+
+    return {
+        update,
+        subscribe,
+    }
+}
