412f831d451b77c8f2cc4899174078c50ecc9308
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-05-21 14:25:54 +0200

    Adding index.html, scripts.js and store.js files

---
+++ scripts.js
@@ -0,0 +1,19 @@
+/*
+* @callback Action
+* @param {State}
+* @returns {State}
+*/
+
+/*
+* @callback Update
+* @param {Action}
+*/
+
+/*
+* @typeof {object} store
+* @props {Update} update
+*/
+
+
+
+
